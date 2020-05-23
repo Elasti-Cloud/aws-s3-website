811d0a81fbf57272cda06612b6270df64ccebcdc
Tinosavr <40817694+Tinosavr@users.noreply.github.com>
2020-05-23 02:02:51 -0400

    tested

--- main.tf
+++ main.tf
@@ -1,20 +1,139 @@
 terraform {
-  required_version = ">=0.12.0"
+  required_version = ">=0.12"
 }
 
 provider "aws" {
-  version = "~> 2.0"
-  profile = var.profile
+  version = ">= 2.0"
   region  = var.region
+  profile = var.profile
 }
 
 # Creating S3 bucket for website's source files
 resource "aws_s3_bucket" "web_bucket" {
-  bucket = var.s3_bucket_name
-  acl    = "private"
+  bucket        = var.s3_bucket_name
+  acl           = "private"
+  force_destroy = var.force_destroy
+  tags          = var.tags
+}
+# Creating S3 bucket for CodePipeline
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket        = "codepipeline-bucket-${var.s3_bucket_name}"
+  acl           = "private"
+  force_destroy = var.force_destroy
+  tags          = var.tags
+}
 
-  provisioner "local-exec" {
-    command = "aws s3 sync ${var.sample_web_site} s3://${var.s3_bucket_name} --profile ${var.profile}"
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-github-s3"
+  tags = var.tags
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline-github-s3-policy"
+  role = aws_iam_role.codepipeline_role.id
+  
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.codepipeline_bucket.arn}",
+        "${aws_s3_bucket.web_bucket.arn}",
+        "${aws_s3_bucket.codepipeline_bucket.arn}/*",
+        "${aws_s3_bucket.web_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_kms_alias" "s3kmskey" {
+  name = "alias/aws/s3"
+}
+
+resource "aws_codepipeline" "codepipeline" {
+  name     = "terraform-github-s3-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  tags     = var.tags
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+
+    encryption_key {
+      id   = data.aws_kms_alias.s3kmskey.arn
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = "Elasti-Cloud"
+        Repo       = "web_site"
+        Branch     = "master"
+        OAuthToken = var.GitHub_token
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "S3"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        BucketName = var.s3_bucket_name
+        Extract    = true
+      }
+    }
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -7,19 +7,30 @@ variable "profile" {
 variable "region" {
   description = "Target AWS region"
   type        = string
-  default     = "us-west-1"
+  default     = "us-east-1"
+}
+
+variable "GitHub_token" {
+  description = "GitHub token to use for AWS CodePipeline"
+  type        = string
 }
 
 variable "s3_bucket_name" {
   description = "S3 bucket name for website's source files. Must be unique."
   type        = string
-  default     = "aws-s3-website-sample001"
+  default     = "aws-s3-website-sample007"
 }
 
 variable "sample_web_site" {
   description = "S3 bucket address with the source code for the web site"
   type        = string
   default     = "s3://wildrydes-us-east-1/WebApplication/1_StaticWebHosting/website"
+}
+
+variable "force_destroy" {
+  description = "Assign True to force deletion of S3 buckets with files on terraform destroy"
+  type        = bool
+  default     = true
 }
 variable "tags" {
   description = "Dict of tags"
